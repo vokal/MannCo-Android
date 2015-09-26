87cc21b68322f9307b74bcb4fae8a55dfc3965d4
David Ryan <dryan31@gmail.com>
2015-09-26 16:31:44 -0500

    started player

--- app/src/main/kotlin/io/vokal/hightower/LeaderBoardActivity.kt
+++ app/src/main/kotlin/io/vokal/hightower/LeaderBoardActivity.kt
@@ -12,9 +12,12 @@ import android.widget.ArrayAdapter
 import android.widget.SimpleAdapter
 import android.widget.Toast
 import com.trello.rxlifecycle.components.RxActivity
+import io.realm.Realm
+import io.realm.RealmConfiguration
 import io.vokal.hightower.api.Api
 import io.vokal.hightower.api.model.Player
 import io.vokal.hightower.api.model.PlayerResponse
+import io.vokal.hightower.api.model.getKdr
 import io.vokal.hightower.api.view.LeaderboardAdapter
 import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 import kotlinx.android.synthetic.activity_leader_board.*
@@ -31,7 +34,7 @@ public class LeaderBoardActivity : RxActivity() {
     companion object {
         public val TAG: String = "LeaderBoardActivity"
         public val killSort = { first : Player, second : Player -> second.KILLS - first.KILLS }
-        public val kdrSort = { first : Player, second : Player -> (second.getKdr()  - first.getKdr()) as Int}
+        public val kdrSort = { first : Player, second : Player -> (second.getKdr()  - first.getKdr()*10) as Int}
         public val pointSort = { first : Player, second : Player -> second.POINTS- first.POINTS}
         public var selectedSort = pointSort
     }
@@ -42,7 +45,8 @@ public class LeaderBoardActivity : RxActivity() {
 
     override protected fun onCreate(state: Bundle?) {
         super.onCreate(state)
-
+        var config : RealmConfiguration= RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(config);
         CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
                 .setFontAttrId(R.attr.fontPath)
                 .build()
@@ -54,6 +58,7 @@ public class LeaderBoardActivity : RxActivity() {
         actionBar.setDisplayShowHomeEnabled(true)
 
         val listener = object : RecyclerView.OnScrollListener() {
+            val listsner = object : Rec
             override fun onScrolled(recyclerView : RecyclerView, dx : Int, dy : Int) {
                 adapter.resetOffset(dy > 0)
             }
@@ -66,6 +71,7 @@ public class LeaderBoardActivity : RxActivity() {
 
         sorting.adapter = ArrayAdapter.createFromResource(this, R.array.names, R.layout.spinner_item)
         timeframe.adapter = ArrayAdapter.createFromResource(this, R.array.times, R.layout.spinner_item)
+        timeframe.visibility = View.INVISIBLE
 
         val sortingListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
@@ -100,7 +106,16 @@ public class LeaderBoardActivity : RxActivity() {
     }
 
     fun updateList(observable : Observable<List<Player>>)  {
-                observable.flatMapIterable({i -> i})
+        observable
+                .doOnNext({list ->
+                    Realm.getDefaultInstance().beginTransaction()
+                    Realm.getDefaultInstance().copyToRealm(list)
+                    Realm.getDefaultInstance().commitTransaction()
+                })
+                .onErrorResumeNext({error ->
+                    Observable.just(Realm.getDefaultInstance().allObjects(Player::class.java))
+                })
+                .flatMapIterable({i -> i})
                 .toSortedList(pointSort)
                 .subscribe(
                         {playerList : List<Player> ->
